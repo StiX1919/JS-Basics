fb771d3b8ffdd2d1c659f854f792f09127d30db9
Spencer Smith <WankFerret@Stevens-MacBook.attlocal.net>
2017-10-10 00:06:59 -0500

    finally commited

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -3,14 +3,24 @@
 var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
-
+function isTyler(name){
+  if (name=='Tyler'){
+    return true
+  }
+  else {
+    return false
+  }
+}
   //Code Here
 
 //////////////////PROBLEM 2////////////////////
 
 
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
-
+function getName(){
+  var firstName = prompt("What is your name?")
+  return firstName
+}
 
   //Code Here
 
@@ -21,7 +31,10 @@ var name = 'Tyler';
 
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
-
+function welcome(){
+var greeting = alert("Welcome, "+ getName())
+return greeting
+}
   //Code Here
 
 
@@ -51,15 +64,20 @@ var name = 'Tyler';
 
 
 //Create a function called myName that returns your name
-
+function myName(){
+  return "Spencer"
+}
   //Code Here
   
 
 
 //Now save the function definition of myName into a new variable called newMyName
-
+function newMyName(){
+  var newMyName = myName()
+  return newMyName
+}
   //Code Here
-
+alert(newMyName())
 //Now alert the result of invoking newMyName
 
 
@@ -69,11 +87,16 @@ var name = 'Tyler';
 
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
-
+function outerFn(){
+  return function(){
+   return "myName"
+  }
+}
   //Code Here
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
+var innerFn = outerFn()
   //Code Here
 
 //Now invoke innerFn.
